2fabe461eefe6324d215b66af44b66ae76ad4c86
danplo1 <budweiser401@wp.pl>
2018-09-10 21:42:32 +0200

    continuation of tasks

--- src/com/company/Task11.java
+++ src/com/company/Task11.java
@@ -10,7 +10,7 @@ public class Task11 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println( "Podaj wybrany przez siebie wyraz z użycie CAPS LOCKa: ");
+        System.out.println( "Podaj wybrany przez siebie wyraz z użyciem CAPS LOCKa: ");
 
         String word = sc.nextLine();
         System.out.println("Zmiana podanego przez Ciebie wyrazy na małe liery, to: " + word.toLowerCase());
