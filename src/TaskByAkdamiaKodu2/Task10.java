5d7583c570c780b582ce0bab3db87bc2492a3930
danplo1 <budweiser401@wp.pl>
2018-09-19 20:25:26 +0200

    continuation of tasks_palindrom

---
+++ src/TaskByAkdamiaKodu2/Task10.java
@@ -0,0 +1,32 @@
+package TaskByAkdamiaKodu2;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+/**
+ * Created by Daniel_D'AGE on 19.09.2018.
+ * Zagadnienie pętle:
+ * Napisz program, który losuje liczby dużego lotka (6 liczb).
+ */
+public class Task10 {
+    public static void main(String[] args) {
+
+
+
+        Random random = new Random();
+
+        int number;
+
+        for (int i = 1; i <= 6; i++) {
+
+            number = random.nextInt(49+1);
+
+            System.out.println(number);
+
+        }
+    }
+}
+
+
+
